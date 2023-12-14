2f4739792960a36345a34708e947a8ef2e400823
ArifBurhanBordemir <76542628+ArifBurhanBordemir@users.noreply.github.com>
2023-12-15 00:37:44 +0300

    a

---
+++ CetContact kopyası/Views/AddContactPage.xaml.cs
@@ -0,0 +1,50 @@
+using CetContact.Model;
+
+namespace CetContact.Views;
+
+public partial class AddContactPage : ContentPage
+{
+    ContactRepository contactRepository;
+	public AddContactPage()
+	{
+		InitializeComponent();
+        contactRepository = new ContactRepository();
+	}
+
+    private void BackButton_Clicked(object sender, EventArgs e)
+    {
+		//Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");
+        //Shell.Current.GoToAsync("//"+nameof(ContactsPage));
+       
+        Shell.Current.GoToAsync("..");
+
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(NameEntry.Text))
+        {
+            // Kullanıcıya uyarı gösterme veya işlemi durdurma
+            await DisplayAlert("Uyarı", "Ad alanını doldurmak zorunludur.", "Tamam");
+            return;
+        }
+        if (string.IsNullOrWhiteSpace(EmailEntry.Text))
+        {
+            // Kullanıcıya uyarı gösterme veya işlemi durdurma
+            await DisplayAlert("Uyarı", "Email alanını doldurmak zorunludur.", "Tamam");
+            return;
+        }
+        ContactInfo contact = new ContactInfo
+        {
+            Name = NameEntry.Text,
+            Phone = PhoneEntry.Text,
+            Address = AdressEntry.Text,
+            Email = EmailEntry.Text,
+        };
+        await contactRepository.AddContact(contact);
+        await Shell.Current.GoToAsync("..");
+
+        
+        
+    }
+}
